c486637498fb10559e1dbfb8f6587b9e0bff63e7
Philipp Czora <philipp@czora.dev>
2023-02-18 00:04:28 +0100

    Sketch for using go libs

--- cmd/main.go
+++ cmd/main.go
@@ -3,40 +3,128 @@ package main
 import (
 	"fmt"
 	"os"
+	"syscall"
+	"time"
 
 	"github.com/gocarina/gocsv"
 	"github.com/pczora/dkb2ynab/formats"
+	"github.com/pczora/dkbrobot/pkg/dkbclient"
+	"golang.org/x/term"
 )
 
 func main() {
-	args := os.Args
-	//TODO: validation
-	path := args[1]
-	var ynabRecords []formats.YnabRecord
-	var inputRecords []formats.InternalRecord
-	ynabConverter := formats.YnabFormatConverter{}
+	var dkbUsername string
+	var dkbPassword string
 
-	converter, err := formats.FindSuitableConverter(path)
-	if err != nil {
-		panic(err)
-	}
-	records, err := converter.ConvertFromFile(path)
-	inputRecords = records
+	fmt.Printf("DKB username: ")
+	_, err := fmt.Scanf("%s", &dkbUsername)
 	if err != nil {
 		panic(err)
 	}
 
-	for _, r := range inputRecords {
-		ynabRecord, err := ynabConverter.ConvertFromInternalRecord(r)
-		if err != nil {
-			panic(err)
+	fmt.Printf("DKB password: ")
+	bytepw, err := term.ReadPassword(syscall.Stdin)
+	if err != nil {
+		os.Exit(1)
+	}
+	fmt.Print("\n")
+
+	dkbPassword = string(bytepw)
+
+	dkb := dkbclient.New()
+
+	err = dkb.Login(dkbUsername, dkbPassword)
+	if err != nil {
+		panic(err)
+	}
+
+	dkbAccounts, err := dkb.ParseOverview()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, a := range dkbAccounts {
+		switch a.AccountType {
+		case dkbclient.CheckingAccount:
+			createDkbCheckingAccountCsv(&dkb, a)
+		case dkbclient.CreditCard:
+			createDkbCreditCardCsv(&dkb, a)
 		}
-		ynabRecords = append(ynabRecords, ynabRecord.(formats.YnabRecord))
-	}
-	marshalled, err := gocsv.MarshalString(ynabRecords)
+		if a.AccountType != dkbclient.CheckingAccount {
+			continue
+		}
 
+	}
+
+}
+
+func createDkbCheckingAccountCsv(dkb *dkbclient.Client, a dkbclient.AccountMetadata) {
+
+	var ynabConverter formats.YnabFormatConverter
+	var records []formats.InternalRecord
+	var ynabRecords []formats.YnabRecord
+
+	transactions, err := dkb.GetAccountTransactions(a, time.Now().Add(30*-time.Hour*24), time.Now())
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, t := range transactions {
+		r := formats.InternalRecord{Date: time.Time(t.Date), ValueDate: time.Time(t.ValueDate), PostingText: t.PostingText, Payee: t.Payee, Purpose: t.Purpose, BankAccountNumber: t.BankAccountNumber, BankCode: t.BankCode, Amount: float64(t.Amount), CreditorID: t.CreditorID, MandateReference: t.MandateReference, CustomerReference: t.CustomerReference}
+		records = append(records, r)
+	}
+
+	for _, r := range records {
+		y, err := ynabConverter.ConvertFromInternalRecord(r)
+		if err != nil {
+			fmt.Println(err)
+		}
+		ynabRecords = append(ynabRecords, y.(formats.YnabRecord))
+	}
+
+	marshalled, err := gocsv.MarshalBytes(ynabRecords)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Print(marshalled)
+
+	err = os.WriteFile(fmt.Sprintf("%v.csv", a.Account), marshalled, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func createDkbCreditCardCsv(dkb *dkbclient.Client, a dkbclient.AccountMetadata) {
+
+	var ynabConverter formats.YnabFormatConverter
+	var records []formats.InternalRecord
+	var ynabRecords []formats.YnabRecord
+
+	transactions, err := dkb.GetCreditCardTransactions(a, time.Now().Add(30*-time.Hour*24), time.Now())
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, t := range transactions {
+		fmt.Printf("%+v\n", t)
+		r := formats.InternalRecord{Date: time.Time(t.Date), ValueDate: time.Time(t.ValueDate), PostingText: t.Purpose, Payee: t.Purpose, Purpose: t.Purpose, BankAccountNumber: "", BankCode: "", Amount: float64(t.Amount), CreditorID: "", MandateReference: "", CustomerReference: ""}
+		records = append(records, r)
+	}
+
+	for _, r := range records {
+		y, err := ynabConverter.ConvertFromInternalRecord(r)
+		if err != nil {
+			fmt.Println(err)
+		}
+		ynabRecords = append(ynabRecords, y.(formats.YnabRecord))
+	}
+
+	marshalled, err := gocsv.MarshalBytes(ynabRecords)
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.WriteFile(fmt.Sprintf("%v.csv", a.Account), marshalled, 0644)
+	if err != nil {
+		panic(err)
+	}
 }
--- formats/formats.go
+++ formats/formats.go
@@ -1,32 +1,25 @@
 package formats
 
 import (
-	"errors"
 	"time"
 )
 
-var converters = []Converter{&DkbRoboConverter{}, &YnabFormatConverter{}, &DkbCreditCardFormatConverter{}, &DkbFormatConverter{}, &DkbRoboCreditCardConverter{}, &ZinspilotFormatConverter{}}
+type DateTime time.Time
 
-type DateTime struct {
-	time.Time
-}
-
-type Amount struct {
-	float64
-}
+type Amount float64
 
 type InternalRecord struct {
-	Date              DateTime `csv:"Date"`
-	ValueDate         DateTime `csv:"Value Date"`
-	PostingText       string   `csv:"Posting Text"`
-	Payee             string   `csv:"Payee"`
-	Purpose           string   `csv:"Purpose"`
-	BankAccountNumber string   `csv:"Bank Account Number"`
-	BankCode          string   `csv:"Bank Code"`
-	Amount            Amount   `csv:"Amount"`
-	CreditorID        string   `csv:"Creditor ID"`
-	MandateReference  string   `csv:"Mandate Reference"`
-	CustomerReference string   `csv:"Customer Reference"`
+	Date              time.Time `csv:"Date"`
+	ValueDate         time.Time `csv:"Value Date"`
+	PostingText       string    `csv:"Posting Text"`
+	Payee             string    `csv:"Payee"`
+	Purpose           string    `csv:"Purpose"`
+	BankAccountNumber string    `csv:"Bank Account Number"`
+	BankCode          string    `csv:"Bank Code"`
+	Amount            float64   `csv:"Amount"`
+	CreditorID        string    `csv:"Creditor ID"`
+	MandateReference  string    `csv:"Mandate Reference"`
+	CustomerReference string    `csv:"Customer Reference"`
 }
 
 type Record interface{}
@@ -37,13 +30,4 @@ type Converter interface {
 	//TODO: This should rather convert from a byte array/a reader/... than from a file
 	ConvertFromFile(path string) ([]InternalRecord, error)
 	Identify(path string) bool
-}
-
-func FindSuitableConverter(path string) (Converter, error) {
-	for _, converter := range converters {
-		if converter.Identify(path) {
-			return converter, nil
-		}
-	}
-	return nil, errors.New("could not find suitable converter")
 }
